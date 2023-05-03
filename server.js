2e8dc1d1c3dbce0b475724e5c3b2fdf96949ac6d
Vikash-Android7500 <Vikash75.jm@gmail.com>
2023-05-03 15:35:23 +0530

    new update

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ mongoose.connect('mongodb://localhost:27017/VikashDatabase', {
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
-.then(() => {console.log("Connection Successful")})
+.then(() => {console.log("Connection is Successful")})
 .catch( (error) => {console.log("Recieved an error")} );
